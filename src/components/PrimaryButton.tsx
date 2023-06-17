63edb988c061b6adf1928d6dcaf50cfe7df57892
moshawa <moshawa0@gmail.com>
2023-06-17 01:07:54 -0400

    config for gh pages

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -10,7 +10,7 @@ export default function PrimaryButton({ text }: PrimaryButtonProps) {
 		<motion.button
 			whileHover={{ scale: 1.02 }}
 			whileTap={{ scale: 0.98 }}
-			transition={{ duration: 0.3, ease: "easeInOut", type: "spring" }}
+			transition={{ duration: 0.3 }}
 			className="flex w-full max-w-xs items-center justify-center gap-4 rounded-full text-white shadow-lg shadow-brand2/70 bg-brand2  py-5 text-lg transition-shadow duration-300 ease-in-out
 			hover:shadow-brand2/70 hover:shadow-xl
 		"
